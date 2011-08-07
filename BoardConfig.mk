00a58fb237e6ccde872a41667f5e23f3a75ab6b2
jerpelea <jerpelea@gmail.com>
2011-08-07 09:17:08 +0300

    semc: added Xperia X10 Mini Pro (mimmi)

---
+++ BoardConfig.mk
@@ -0,0 +1,6 @@
+-include device/semc/msm7x27-common/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := mimmi
+TARGET_SPECIFIC_HEADER_PATH := device/semc/mimmi/include
+
+-include device/semc/msm7x27-common/Android.mk
---
+++ device_mimmi.mk
@@ -0,0 +1,44 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/semc/mimmi/mimmi-vendor.mk)
+
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := U20i
+PRODUCT_DEVICE := mimmi
+PRODUCT_MODEL := U200i
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/semc/mimmi/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+-include device/semc/msm7x27-common/msm7x27.mk
+
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
+DEVICE_PACKAGE_OVERLAYS += device/semc/mimmi/overlay
+
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES += \
+    device/semc/mimmi/prebuilt/media_profiles.xml:system/etc/media_profiles.xml 
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/mimmi/prebuilt/bootrec:root/sbin/bootrec
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=120 \
+
+# mimmi uses high-density artwork where available
+PRODUCT_LOCALES += ldpi
+
+
